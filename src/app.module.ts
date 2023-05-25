a70a0c4ca9974a41ee982d259508d80c498ab3ea
AbbyIsCoding <abigail_rochelle24@milton.edu>
2023-05-25 10:31:09 -0400

    make it live

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { characterModule } from './characters/char.module';
-import { speciesModule} from './species/spec.module'; 
+//import { speciesModule} from './species/spec.module'; 
 import { MongooseModule } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+
+
+
 
 @Module({
-  imports: [characterModule, speciesModule, MongooseModule.forRoot('mongodb+srv://AbbyIsOnAtlas:aksjdf3u8@cluster0.e5gaho1.mongodb.net/test')],
+  imports: [characterModule, MongooseModule.forRoot('mongodb+srv://AbbyIsOnAtlas:aksjdf3u8@cluster0.e5gaho1.mongodb.net/test')],
   controllers: [AppController], // control how you handle incoming requests 
   providers: [AppService], // extra classes which you can inject into controllers or another provider 
 })
